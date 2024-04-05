cd27a869afd4d652d61f583044df195f4e6793df
Shreyansh Gupta <120119737+shrey7ansh07@users.noreply.github.com>
2024-04-06 02:11:14 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api/v1': 'http://localhost:8000'
+      '/api/v1': 'https://finwiser-backend.vercel.app'
     },
 
   }
